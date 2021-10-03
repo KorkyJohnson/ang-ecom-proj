f5f2d728fbedc4bec94001d2f28e6889f2c052df
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2021-10-03 15:21:39 -0400

    added CrossOrigin support

--- 02-backend/spring-boot-ecomm/spring-boot-ecomm/src/main/java/com/luv2code/ecomm/dao/iProductCategoryRepository.java
+++ 02-backend/spring-boot-ecomm/spring-boot-ecomm/src/main/java/com/luv2code/ecomm/dao/iProductCategoryRepository.java
@@ -4,7 +4,9 @@ import com.luv2code.ecomm.entity.ProductCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 //                                               JSON Entry         path /product-category
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface iProductCategoryRepository extends JpaRepository<ProductCategory, Long> {    
--- 02-backend/spring-boot-ecomm/spring-boot-ecomm/src/main/java/com/luv2code/ecomm/dao/iProductRepository.java
+++ 02-backend/spring-boot-ecomm/spring-boot-ecomm/src/main/java/com/luv2code/ecomm/dao/iProductRepository.java
@@ -3,6 +3,20 @@ package com.luv2code.ecomm.dao;
 import com.luv2code.ecomm.entity.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+/*
+    SpringBoot  = port 8080
+    Angular     = port 4200
+
+    Origin          = protocol  + hostname  + port  =   protocol    + hostname      + port   
+
+    same origin     = http://   + localhost + :4200 =   http://     + localhost     + :4200
+
+    !same origin    = http://   + localhost + :4200 =   http://     + localhost     + :8080
+*/
+// if you don't use CrossOrigin you will get an error!!!! ***IMPORTANT***
+// Accept calls from web browser from this origin
+@CrossOrigin("http://localhost:4200") // Where our Angular application is running on
 //                                                      Product Entity, Long (key ID)
 public interface iProductRepository extends JpaRepository<Product, Long>{    
 }
