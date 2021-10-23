a413077ccf4502f73ccb49e1092f49800ee4e3e2
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2021-10-23 13:13:36 -0400

    defined routes

--- 03-frontend/angular-ecomm/src/app/app.module.ts
+++ 03-frontend/angular-ecomm/src/app/app.module.ts
@@ -6,12 +6,25 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
 
+import {Routes, RouterModule} from '@angular/router';
+
+// Order of routes is important (top --> down checking)
+
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
-  imports: [
+  imports: [ 
+    RouterModule.forRoot(routes), // routes is our const we defined previous w/ our routing paths
     BrowserModule,
     HttpClientModule
   ],
