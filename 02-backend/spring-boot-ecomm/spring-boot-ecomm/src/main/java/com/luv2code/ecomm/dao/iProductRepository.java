3b9dfcbc1df02f214f568268d06d8c458befa914
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2021-10-23 23:36:35 -0400

    add findByCategoryId method

--- 02-backend/spring-boot-ecomm/spring-boot-ecomm/src/main/java/com/luv2code/ecomm/dao/iProductRepository.java
+++ 02-backend/spring-boot-ecomm/spring-boot-ecomm/src/main/java/com/luv2code/ecomm/dao/iProductRepository.java
@@ -2,8 +2,11 @@ package com.luv2code.ecomm.dao;
 
 import com.luv2code.ecomm.entity.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 /*
     SpringBoot  = port 8080
     Angular     = port 4200
@@ -19,4 +22,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200") // Where our Angular application is running on
 //                                                      Product Entity, Long (key ID)
 public interface iProductRepository extends JpaRepository<Product, Long>{    
+
+    // findBy is a Query Method based (in this case) on the 'id' parameter
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
